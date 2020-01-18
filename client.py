a38004b18c5f1c5c2609779eb9afe76541c22af3
aayush kubitkar <kubitkar.a@husky.neu.edu>
2020-01-17 22:00:46 -0500

    working for multiple clients

--- client.py
+++ client.py
@@ -5,39 +5,76 @@ PORT = 22300
 HEADERSIZE=5
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-
 #Client Hello
-# msg_tag = 0
-# major_v = 1
-# minor_v = 1
-# user_agent_string="Hi server"
-# client_hello = pack('>3BH', msg_tag, major_v, minor_v, len(user_agent_string))+bytes(user_agent_string,"utf-8")
-# s.send(client_hello)
-# recv_buff = bytearray(1024)
-# nbytes= s.recv_into(recv_buff)
-# length_of_msg= unpack('>H',recv_buff[3:5])
-# print (recv_buff[3:length_of_msg[0]+5])
-
-#Ping Request
-ping_tag = 3
-a=[1,2,4]
-hash_alg=0
-ping_req = pack('>BH',ping_tag,len(a))+bytes(a)+pack('>B',hash_alg)
-s.send(ping_req)
+msg_tag = 0
+major_v = 0
+minor_v = 1
+user_agent_string="Hi server"
+client_hello = pack('>3BH', msg_tag, major_v, minor_v, len(user_agent_string))+bytes(user_agent_string,"utf-8")
+s.send(client_hello)
 recv_buff = bytearray(1024)
 nbytes= s.recv_into(recv_buff)
-length_of_msg= unpack('>H',recv_buff[1:3])
-print (length_of_msg[0])
-print("received: ", recv_buff[3:length_of_msg[0]+3])
+length_of_msg= unpack('>H',recv_buff[3:5])
+print (recv_buff[3:length_of_msg[0]+5])
+
+s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s2.connect((HOST, PORT))
+s3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s3.connect((HOST, PORT))
+s4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s4.connect((HOST, PORT))
+s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s1.connect((HOST, PORT))
+msg_tag = 0
+major_v = 1
+minor_v = 1
+user_agent_string="Hi server"
+client_hello = pack('>3BH', msg_tag, major_v, minor_v, len(user_agent_string))+bytes(user_agent_string,"utf-8")
+s1.send(client_hello)
+data =s1.recv(1024)
+print (data)
+
+store_tag=7
+key="a"
+value=[1,2,4]
+store_req = pack('>BH', store_tag, len(key.encode()))+bytes(key,"utf-8")+pack('>H',len(value))+bytes(value)
+s1.send(store_req)
+data =s1.recv(1024)
+print (data)
+
+load_tag=5
+key="a"
+load_req = pack('>BH',load_tag, len(key.encode()))+bytes(key,"utf-8")
+s1.send(load_req)
+# recv_buffer = bytearray(1024)
+# nbytes= s.recv_into(recv_buffer)
+# length_of_value=unpack('>H',recv_buffer[1:3])
+# print (recv_buffer)
+# print ("received: ", recv_buffer[3:length_of_value[0]+3])
+data =s1.recv(1024)
+print (data)
+#Ping Request
+# ping_tag = 3
+# a=[1,2,4]
+# hash_alg=0
+# ping_req = pack('>BH',ping_tag,len(a))+bytes(a)+pack('>B',hash_alg)
+# s1.send(ping_req)
+# recv_buff1 = bytearray(1024)
+# n1bytes= s1.recv_into(recv_buff1)
+# length_of_msg1= unpack('>H',recv_buff1[1:3])
+# print (length_of_msg1[0])
+# print("received: ", recv_buff1[3:length_of_msg1[0]+3])
 
 # Store Request
-# store_tag=7
-# key="a"
-# value=[1,2,3]
-# store_req = pack('>BH', store_tag, len(key.encode()))+bytes(key,"utf-8")+pack('>H',len(value))+bytes(value)
-# s.send(store_req)
+store_tag=7
+key="a"
+value=[1,2,3]
+store_req = pack('>BH', store_tag, len(key.encode()))+bytes(key,"utf-8")+pack('>H',len(value))+bytes(value)
+s.send(store_req)
+data =s.recv(1024)
+print (data)
 # recv_buff = bytearray(1024)
-# nobytes= s.recv_into(recv_buff)
+# nobytes= s1.recv_into(recv_buff)
 # print (recv_buff)
 # length_of_hash=unpack('>H',recv_buff[1:3])
 # print ("received: ", recv_buff[3:length_of_hash[0]+3])
@@ -47,12 +84,14 @@ print("received: ", recv_buff[3:length_of_msg[0]+3])
 
 
 #Load Request
-# load_tag=5
-# key="a"
-# load_req = pack('>BH',load_tag, len(key.encode()))+bytes(key,"utf-8")
-# s.send(load_req)
+load_tag=5
+key="a"
+load_req = pack('>BH',load_tag, len(key.encode()))+bytes(key,"utf-8")
+s.send(load_req)
 # recv_buffer = bytearray(1024)
 # nbytes= s.recv_into(recv_buffer)
 # length_of_value=unpack('>H',recv_buffer[1:3])
 # print (recv_buffer)
 # print ("received: ", recv_buffer[3:length_of_value[0]+3])
+data =s.recv(1024)
+print (data)
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
+import select
 import hashlib
 from struct import *
+
 def handleClientHello(clientsocket, recv_buff):
     print ("inside clienthello")
     if recv_buff:
@@ -9,17 +11,15 @@ def handleClientHello(clientsocket, recv_buff):
         if version[0]!=1:
             print("client not supported with version", version[0])
             errorResponse(clientsocket, "Client version not compliant")
-            return False
-        #userAgentString = recv_buff[5:length[0]+5].decode("utf-8")
-        #print (payload)
+            return
         serverHello(clientsocket)
-        return True
+
 def serverHello(clientsocket):
     print ("inside server hello")
     minor_v = 1
     userAgentString="Hi Client"
     res = pack('>BBBH', 1, 1, minor_v, len(userAgentString))+bytes(userAgentString, "utf-8")
-    clientsocket.send(res)
+    responseMessage[clientsocket] = res
 
 def handlePingReq(clientsocket, recv_buff):
     print ("inside ping req")
@@ -29,25 +29,25 @@ def handlePingReq(clientsocket, recv_buff):
     print (data)
     hash_alg = unpack('>B', recv_buff[length[0]+3:length[0]+4])
     print (hash_alg[0])
-    return pingResponse(clientsocket, data, hash_alg[0])
+    pingResponse(clientsocket, data, hash_alg[0])
+
 def pingResponse(clientsocket,data,hash_alg):
     print("inside pingResponse")
 
     if hash_alg==0:
         res = pack('>BH', 4, len(data))+bytes(data)
-        clientsocket.send(res)
-        return True
+        responseMessage[clientsocket] = res
+        return
     elif hash_alg==1:
         hashVal= hashlib.sha256(data)
     elif hash_alg==2:
         hashVal= hashlib.sha512(data)
     else:
         errorResponse(clientsocket, "Invalid hash algorithm")
-        return False
+        return
     hash_data=hashVal.digest()
     res = pack('>BH', 4, len(hash_data))+bytes(hash_data)
-    clientsocket.send(res)
-    return True
+    responseMessage[clientsocket] = res
 
 def handleStoreReq(clientsocket, recv_buff):
     print ("inside handleStoreReq")
@@ -55,15 +55,15 @@ def handleStoreReq(clientsocket, recv_buff):
     key=recv_buff[3:lengthOfKey[0]+3].decode("utf-8")
     lengthOfValue=unpack('>H',recv_buff[lengthOfKey[0]+3:lengthOfKey[0]+5])
     value=recv_buff[lengthOfKey[0]+5:lengthOfKey[0]+5+lengthOfValue[0]]
-    valueStore[key]=value
-    storeResponse(clientsocket,value)
-    return True
-def storeResponse(clientsocket, value):
+    valueStore[clientsocket]={key:value}
+    storeResponse(clientsocket,key)
+
+def storeResponse(clientsocket, key):
     print ("inside storeResponse")
-    hashVal= hashlib.sha256(value)
+    hashVal= hashlib.sha256(valueStore[clientsocket][key])
     hash_data = hashVal.digest()
     res = pack('>BH', 8, len(hash_data)) + hash_data + pack('B', 1)
-    clientsocket.send(res)
+    responseMessage[clientsocket] = res
 
 def handleLoadReq(clientsocket, recv_buff):
     print("inside handleLoadReq")
@@ -71,65 +71,98 @@ def handleLoadReq(clientsocket, recv_buff):
     key = recv_buff[3:lengthOfKey[0] + 3].decode("utf-8")
     if key:
         loadResponse(clientsocket, key)
-        return True
     else:
         errorResponse(clientsocket, "Key is null")
         return False
+
 def loadResponse(clientsocket, key):
     print("inside loadResponse")
     print (key)
-    if key in valueStore:
-        value=valueStore[key]
-    else:
+    try:
+        value=valueStore[clientsocket][key]
+    except KeyError:
         value=[]
     print (value, len(value))
     res = pack('>BH', 6, len(value))+bytes(value)
-    clientsocket.send(res)
+    responseMessage[clientsocket] = res
+
 def errorResponse(clientsocket, msg):
     res = pack('>BH', 2, len(msg))+ bytes(msg, "utf-8")
-    clientsocket.send(res)
-    return False
+    responseMessage[clientsocket] = res
+    incoming.remove(clientsocket)
 
 RECV_BUFFER_SIZE = 1024
 HOST = '0.0.0.0'
 PORT = 22300
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(2)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(5)
+
+incoming=[server]
+outgoing=[]
+responseMessage={}
+clientInit={}
 valueStore={}
-response=None
-new_msg = True
-while True:
-    print("waiting for connection...")
-    clientsocket, addr = s.accept()
-    print("new connection received")
-    while True:
-        print("connected by", addr)
-        recv_buff = bytearray(RECV_BUFFER_SIZE)
-        numberOfBytes = clientsocket.recv_into(recv_buff)
-        if numberOfBytes!=0:
-            tag = unpack('>B', recv_buff[:1])
-            if new_msg and tag[0] != 0:
-                response = errorResponse(clientsocket, "Client Not Initialized")
-            else:
-                new_msg = False
-                switcher = {
-                    0: handleClientHello,
-                    3: handlePingReq,
-                    5: handleLoadReq,
-                    7: handleStoreReq
-                }
-                print(tag[0])
-                func = switcher.get(tag[0])
-                response = func(clientsocket, recv_buff)
-                print(response)
-            if not response:
-                # clientsocket.close()
-                break
+
+while incoming:
+    print("start waiting")
+    reads, writes, exceptions = select.select(incoming, outgoing, incoming)
+    for s in reads:
+        if s is server:
+            print("connection accepted from", s)
+            clientsocket, addr = server.accept()
+            clientsocket.setblocking(False)
+            incoming.append(clientsocket)
         else:
-            break
-
-
-
-
-
+            print("data read from socket", s)
+            recv_buff = bytearray(RECV_BUFFER_SIZE)
+            numberOfBytes = s.recv_into(recv_buff)
+            if numberOfBytes != 0:
+                outgoing.append(s)
+                tag = unpack('>B', recv_buff[:1])
+                if  tag[0] != 0:
+                    try:
+                        if clientInit[s]:
+                            switcher = {
+                                3: handlePingReq,
+                                5: handleLoadReq,
+                                7: handleStoreReq
+                            }
+                            print(tag[0])
+                            func = switcher.get(tag[0])
+                            func(s, recv_buff)
+                    except KeyError:
+                        print (tag[0])
+                        errorResponse(s, "Client Not Initialized")
+                elif tag[0]==0:
+                    handleClientHello(s, recv_buff)
+                    clientInit[s] = True
+            else:
+                print ("received no data")
+                incoming.remove(s)
+                s.close()
+                if s in responseMessage:
+                    del responseMessage[s]
+                if s in clientInit:
+                    del clientInit[s]
+                if s in valueStore:
+                    del valueStore[s]
+    for s in writes:
+        try:
+            print ("writing data to socket",s, responseMessage[s])
+            s.send(responseMessage[s])
+            outgoing.remove(s)
+        except:
+            outgoing.remove(s)
+    for s in exceptions:
+        print ("inside exceptions")
+        if s in outgoing:
+            outgoing.remove(s)
+        incoming.remove(s)
+        s.close()
+        if s in responseMessage:
+            del responseMessage[s]
+        if s in clientInit:
+            del clientInit[s]
+        if s in valueStore:
+            del valueStore[s]
